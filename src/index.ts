2cb4c6b3a17d96171ac0e9661e9915dc6b0fcb93
Tiphaine <tiphainedurand44@gmail.com>
2024-07-03 10:52:12 +0200

    🔧 Update tsjson

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+console.log('Hello, world');
+//On lance le script quand la page est chargée
+document.addEventListener('DOMContentLoaded', function() { 
+
+  //On récupère l'élément avec la classe title
+  const myTitle = document.querySelector('.title') as HTMLElement
+  
+  //On change la couleur du texte en rouge
+  myTitle.style.color = 'blue' 
+
+});
