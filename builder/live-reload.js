1b67b63508c4d3e4796d74c39303143dff056550
Tiphaine <tiphainedurand44@gmail.com>
2024-07-03 12:13:08 +0200

    🔊 Dev mode console log

--- builder/live-reload.js
+++ builder/live-reload.js
@@ -1,6 +1,8 @@
 const EVENT_TYPE = 'change';
 const eventSourceUrl = `${SERVE_ORIGIN}/esbuild`;
 
+console.log(`Dev Mode on ${SERVE_ORIGIN}`);
+
 const eventSource = new EventSource(eventSourceUrl);
 eventSource.addEventListener(EVENT_TYPE, () => {
   console.log('Change detected, reloading page...');
